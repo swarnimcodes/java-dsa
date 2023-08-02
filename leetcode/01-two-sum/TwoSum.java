e69325eb785c3e83c6a60a5fcae2cd1001da0739
Swarnim B <swarnim14.9@hotmail.com>
2023-08-02 17:11:09 +0530

    two sum completed via brute force method

--- leetcode/01-two-sum/TwoSum.java
+++ leetcode/01-two-sum/TwoSum.java
@@ -1,13 +1,5 @@
-//package leetcode.01-two-sum;
-
 public class TwoSum {
-
-    public static void main(String[] args) {
-        int[] nums = {2, 7, 11, 15};
-        int target = 9;
-        twosum(nums, target);
-    }
-        public int[] twosum(int[] nums, int target) {
+    public static int[] twosum(int[] nums, int target) {
         int n = nums.length;
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
@@ -17,5 +9,14 @@ public class TwoSum {
             }
         }
         return new int[]{};
+    }
+    public static void main(String[] args) {
+        int[] nums = {22, 24, 2, 7, 11, 15};
+        int target = 9;
+        int[] ans = {0,1};
+        ans = twosum(nums, target);
+        for (int i = 0; i < ans.length; i++) {
+        System.out.println(ans[i]);
+        }
     }
 }
